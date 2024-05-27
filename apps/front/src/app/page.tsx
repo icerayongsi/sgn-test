fec3972cd203fdc871b31750d52731ffe7ccba95
icerayongsi <icerayongsi@gmail.com>
2024-05-27 19:42:42 +0700

    init

--- apps/front/src/app/page.tsx
+++ apps/front/src/app/page.tsx
@@ -1,11 +1,7 @@
 import styles from './page.module.css';
 
 export default function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.css file.
-   */
+
   return (
     <div className={styles.page}>
       <div className="wrapper">
